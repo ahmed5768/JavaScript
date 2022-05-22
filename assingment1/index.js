a81f8235d02397a2c2a5ef98f93e91a61d2b9076
Ahmed Ashfaq <m.ahmedashfaq1@gmail.com>
2022-05-23 02:25:10 +0500

    assingment1

---
+++ assingment1/index.js
@@ -0,0 +1,408 @@
+// Chapter no 1
+
+// Task no 1
+
+// alert("Error! please enter avalid password");
+
+// Task no 2
+
+// alert("Welcome to js land...\nHappy coding! ")
+
+// Task no 3
+
+// alert("Welcome to js land...")
+
+// Task no 4
+
+// alert("Happy coding!\nPevent this page from creating additional dialogs")
+
+// task:05
+
+//  var message = "Hello...! i can run  js through my web browser's console"
+//  console.log(message)
+
+// Chapter no 2
+
+// Task no 1
+
+// var username ="Ahmed Ashfaq";
+// var message ="Hello world";
+// alert(username);
+// alert(message);
+
+// Task no 2
+
+// alert("Pizza\nPizz\nPiz\nPi\nP");
+
+// Task no 3
+
+// var email ="My email address (m.ahmedashfaq1@gmail.com)";
+// alert(email);
+
+// Task no 4
+
+// var book ="A smarter way to learn javascript"
+// alert("I am trying to learn from the book " + book)
+
+// Task no 5 
+
+// document.write("<b>Yah! I can write HTML</b> content through Javascript")
+
+// Task no 6
+
+// var pattern ="▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬";
+// alert(pattern)
+
+// Chapter no 3
+
+// Task no 1
+
+// var age = 18
+// alert("I am" + " " + age + " " + "years old")
+
+// Task no 2
+
+// var uservisitors = 14;
+// alert("You have visited this site" + " " + uservisitors + " " + "times")
+
+// Task no 3
+
+// var birthyear = 2003;
+// document.write("My birth year is" + " " + birthyear + "<br> " + "Data type of my declared variable is" + " " + typeof (birthyear))
+
+// Task no 4
+
+// var vname = "Ahmed Ashfaq"
+// var Product = "Tracksuit"
+// var qty = 1
+// var storename = "Outfiter.pk"
+// alert(vname + " " + "ordered" + " " + qty + " " + Product + " " + "on " + storename)
+
+// chapter no 4
+
+// task no 1
+
+// var my, age, store;
+
+// Task no 2
+
+// legal variables
+// var myName = Ahmed;
+// var my_Name = Ahmed;
+// var my_Name1 = Ahmed;
+// var myname = Ahmed;
+// var _my_Ahmed = Ahmed;
+
+// illegal
+// var my-name = Ahmed;
+// var 1myname = Ahmed;
+// var @nyname = Ahmed;
+// var my name = Ahmed;
+// var my&name = Ahmed;
+
+// Task no 3
+
+// document.write("<h2>Rules for naming JS variables</h2>");
+// document.write("a) Variable names can only contain , numbers, $ and _. For Example: <b>$my_1stVariable.</b> </br>")
+// document.write("b) Variable must begin with a letter, $ or _. For Example: <b>$name, _name</b> or <b>name.</b> </br>")
+// document.write("c) Variable name are case sensitive. </br>")
+// document.write("d) Variable name should not be JS keywords.")
+
+// Chapter no 5
+
+// Task no 1
+
+// var num1 = 36;
+// var num2 = 28;
+// var sum = num1 + num2
+// document.write("Sum of " + num1 + " and " + num2 + " is " + sum)
+
+// Task no 2
+
+// var num1 = 36;
+// var num2 = 28;
+// var sub = num1 - num2
+// var div = num1 / num2
+// var mul = num1 * num2
+// var mod = num1 % num2
+
+// document.write("Subtraction of " + num1 + " and " + num2 + " is " + sub +"<br>");
+// document.write("Division of " + num1 + " and " + num2 + " is " + div + "<br>")
+// document.write("Multiplication of " + num1 + " and " + num2 + " is " + mul + "<br>")
+// document.write("Reminder of " + num1 + " and " + num2 + " is " + mod )
+
+// Task no 3
+
+// var num1;
+// document.write("Value after variable declaration is:" + num1 + "<br>");
+
+// num1 = 5
+// document.write("Initial value: " + num1 + "<br>" );
+
+// num1++
+// document.write("Value after increment is: " + num1 + "<br>")
+
+// num1=num1 + 7
+// document.write("Value after addition is: " + num1 + "<br>")
+
+// --num1
+// document.write("Value of decrement is: " + num1 + "<br>")
+
+// num1 = num1 % 3
+// document.write("The remainder is: " + num1)
+
+// Task no 4
+
+// var tp = 600
+// var not = 5
+// var totalPKR = tp * not
+// document.write("Total cost to buy " + not + " ticket to a movie is " + totalPKR + "PKR")
+
+// Task no 5
+
+// var table = 4
+// document.write("<h2>Table of 4</h2>")
+// document.write("4 x 1= " + 4 * 1 + " <br>" + "4 x 2= " + 4 * 2 + " <br>" + "4 x 3= " + 4 * 3 + " <br>" + "4 x 4= " + 4 * 4 + " <br>" + "4 x 5= " + 4 * 5 + " <br>" + "4 x 6= " + 4 * 6 + " <br>" + "4 x 7= " + 4 * 7 + " <br>" + "4 x 8= " + 4 * 8 + " <br>" + "4 x 9= " + 4 * 9 + " <br>" + "4 x 10= " + 4 * 10 + " <br>")
+
+// Task no 6
+
+// var celsius =25;
+// var fahrenheit =70;
+
+// C=(fahrenheit-32)*5/9
+// F=(celsius*9/5)+32
+
+// document.write(celsius + "<sup>o</sup>C is " + F + "<sup>o</sup>F" + "<br>")
+// document.write(fahrenheit + "<sup>o</sup>F is " + C + "<sup>o</sup>C")
+
+// Task no 7
+
+// var item1 = 3000
+// var item2 = 6000
+// var item1qty = 3
+// var item2qty = 4
+// var shipchar = 200
+
+// var item1p = item1 * item1qty
+// var item2p = item2 * item2qty
+
+// var subto = item1p + item2p
+// var total = item1p + item2p + shipchar
+
+// document.write("<h1>Your Order</h1>")
+// document.write("PRODUCT ----------------------------- SUBTOTAL" + "<br>")
+// document.write("Smart Watch (item1) × " + item1qty +  " ----------------- " + item1p + "PKR" + "<br>")
+// document.write("Mobile (item2) x " + item2qty + " ----------------------- " + item2p + "PKR" + "<br>")
+// document.write("Subtotal ------------------------------------ " + subto + "PKR" + "<br>")
+// document.write("Shipping Charges ---------------------------- " + shipchar + "PKR" + "<br>")
+// document.write("Total cost of your order is : -------------- " + total + "PKR")
+
+// Task no 8 
+
+// var totalmarks = 980
+// var marksobt = 804
+// var per = marksobt / totalmarks * 100
+
+// document.write ("<h1> Mark Sheet</h1>")
+// document.write ("Total Marks: " + totalmarks + "<br>")
+// document.write ("Marks Obtained: " + marksobt + "<br>")
+// document.write ("Percentage: " + per)
+
+// Task no 8
+
+// var oneusd = 200.10
+// var onesaudi = 53.34
+
+// var PKR1 = oneusd * 10
+// var PKR2 = onesaudi * 10
+// var total = PKR1 + PKR2
+
+// document.write ("<h1> Currency In PKR</h1>")
+// document.write ("Total currency in PKR: " + total)
+
+// Task no 10 
+
+// var single = 5 * 10 /2
+// document.write ("Single Expression calculation is: " + single)
+
+// Task no 11
+
+// var cyear = 2022
+// var birth = 2003
+// var age = 2022 - 2003
+// document.write("<h1> Age Calculator </h1>")
+// document.write("Current Year: " + cyear + "<br>")
+// document.write("Birth Year: " + birth + "<br>")
+// document.write("My age: " + age)
+
+// Task no 12
+
+// var rad = 20
+// var last = 3.142
+// var cir = 2 * last * rad
+// var area = last * rad * rad
+
+// document.write("<h1>The Geometrizer</h1>")
+// document.write("Radius of a circle is: " + rad + "<br>")
+// document.write("The circumference is: " + cir + "<br>")
+// document.write("The area is: " + area)
+
+// Task no 13
+
+// var favsn = "CHOCOLATTO"
+// var curage = 18
+// var maxage = 65
+// var persna = 4
+// var agecal = maxage - curage
+// var totalsna = agecal * persna
+
+// document.write("<h1>The Lifetime Supply Calculator</h1>")
+// document.write("Favourite Snacks: " + favsn + "<br>")
+// document.write("Curent Age: " + curage + "<br>")
+// document.write("Estimated Maximum Age: " + maxage + "<br>")
+// document.write("Amount Of Snack Per Day: " + persna + "<br>")
+// document.write("You Will Need " + totalsna + " " + favsn + " To Last You Until The Ripe Old Age Of " + maxage)
+
+// Chapter no 6-9
+
+// Task no 1
+
+// var a = 10
+// document.write("Result:" + "<br>" + "The value of a is: " + a + "<br>" + "------------------------------------------" + "<br><br>")
+
+// ++a
+// document.write("The value of ++a is: " + a + "<br>" + "Now the value of a is: " + a + "<br><br>")
+
+
+// document.write("The value of a++ is: " + a + "<br>" + a++ +"Now the value of a is: " + a + "<br><br>" )
+
+// --a
+// document.write("The value of --a is: " + a + "<br>" + "Now the value of a is: " + a + "<br><br>")
+
+
+// document.write("The value of a-- is: " + a + "<br>" + a-- + "Now the value of a is: " + a )
+
+// Task no 2
+
+// var a=2,b=1
+// var result = --a - --b + ++b + b--
+//             // 1 - 0 + 1 + 1
+//             // 1 + 1 + 1 
+//             // 3
+// document.write("Explaination");
+// document.write("<br>");
+// document.write("------------------------------------------");
+// document.write("<br>");
+// document.write("--a is the pre decrement of a, current value of a is 2 after decrement --a =  1  <br>");
+// document.write("--b is the pre decrement of b, current value of b is 0 after decrement --b =  0  <br>");
+// document.write("++b is the pre increment of b, current value of b is 1 after increment ++b =  1  <br>");
+// document.write("++b is the post decrement of b, it will decrease the value after call the varibale current value of b is 1 after decrement  b-- =  1" + "<br><br>");
+
+// document.write("Current value of a is : 1" + "<br>");
+// document.write("Current value of b is : 1" + "<br>");
+// document.write("Result : " + result);
+
+// Task no 3
+
+// var name = prompt("Enter your name")
+// document.write("welcome " + name)
+
+// Task no 4
+
+// var tabno = prompt("Enter any number")
+// document.write(tabno + " x 1 = " + tabno * 1 + "<br>" + tabno + " x 2 = " + tabno * 2 + "<br>" + tabno + " x 3 = " + tabno * 3 + "<br>" + tabno + " x 4 = " + tabno * 4 + "<br>" + tabno + " x 5 = " + tabno * 5 + "<br>" + tabno + " x 6 = " + tabno * 6 + "<br>" + tabno + " x 7 = " + tabno * 7 + "<br>" + tabno + " x 8 = " + tabno * 8 + "<br>" + tabno + " x 9 = " + tabno * 9 + "<br>" + tabno + " x 10 = " + tabno * 10)
+
+// Task no 5
+
+// var namesub1 = prompt("Enter a your subject name")
+// var smark1 = parseInt (prompt("Enter Obtained marks of this subject"))
+
+// var namesub2 = prompt("Enter a your subject name")
+// var smark2 =  parseInt (prompt("Enter Obtained marks of this subject"))
+
+// var namesub3 = prompt("Enter a your subject name")
+// var smark3 = parseInt (prompt("Enter Obtained marks of this subject"))
+
+// var eamark = 100
+// var eacper1 = smark1 / eamark * 100
+// var eacper2 = smark2 / eamark * 100
+// var eacper3 = smark3 / eamark * 100
+
+// var tomarksub = 300
+// var obtmarks = smark1 + smark2 + smark3
+// var per = obtmarks / tomarksub * 100
+
+// document.write(`<table>
+// <tr>
+//     <th>Subject</th>
+//     <th>Total Marks</th>
+//     <th>Obtained Marks</th>
+//     <th>Percentage</th>
+// </tr>
+
+// <tr>
+//     <td>${namesub1}</td>
+//     <td>${eamark}</td>
+//     <td>${smark1}</td>
+//     <td>${eacper1}</td>
+// </tr>
+
+// <tr>
+//     <td>${namesub2}</td>
+//     <td>${eamark}</td>
+//     <td>${smark2}</td>
+//     <td>${eacper2}</td>
+// </tr>
+
+// <tr>
+//     <td>${namesub3}</td>
+//     <td>${eamark}</td>
+//     <td>${smark3}</td>
+//     <td>${eacper3}</td>
+// </tr>
+
+// <tr>
+//     <td></td> 
+//     <td>${tomarksub}</td>
+//     <td>${obtmarks}</td>
+//     <td>${per}</td>
+// </tr>
+// </table>`)
+
+// task:05
+
+var sub1 = "Math";
+var sub2 = "English";
+var sub3 = "Urdu";
+var totalmarks = 100;
+var Obtainedmarks1 = 90;
+var Obtainedmarks2 = 76;
+var Obtainedmarks3 = 66;
+var percent1 = Obtainedmarks1 / totalmarks * 100;
+var percent2 = Obtainedmarks2 / totalmarks * 100;
+var percent3 = Obtainedmarks3 / totalmarks * 100;
+var total = 3 * totalmarks;
+var totalobtained = Obtainedmarks1 + Obtainedmarks2 + Obtainedmarks3;
+var totalpercent = totalobtained / total * 100;
+
+document.write("<h3> subjects"  + " " +  "Totalmarks" + " " + "obtainedmarks" + " " + "Percentage </h3>")
+
+document.write(sub1 + `\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0` + totalmarks + `\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0` + Obtainedmarks1 + `\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0`+ percent1 + "%"  )
+
+document.write("<br><br>")
+
+document.write(sub2 + `\xa0\xa0\xa0\xa0\xa0` + totalmarks + `\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0` + Obtainedmarks2 + `\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0`+ percent2 + "%")
+
+document.write("<br><br>")
+
+document.write(sub3 + `\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0` + totalmarks + `\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0` + Obtainedmarks3 + `\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0`+ percent3 + "%")
+
+document.write("<br><br>")
+document.write("-------------------------------------------------------------------------------------")
+document.write("<br>")
+
+document.write(`\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0` + total + `\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0` + totalobtained + `\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0` + totalpercent + "%")
+
+// var str = new String("Demo Text");
+//          document.write(str.bold());
+//          alert(str.bold());
